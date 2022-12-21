49fa4dc32c63edfe231a88d4c271ce5266fd8ce3
Santiago Rodriguez Palacio <santrodriguezp@correo.udistrital.edu.co>
2022-12-21 08:49:18 -0500

    v2

--- Programa.py
+++ Programa.py
@@ -1,5 +1,6 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, request, redirect, url_for
 from db import db
+from Apartamento import Apartamento
 
 class Programa:
     
@@ -8,14 +9,39 @@ class Programa:
         self.app=Flask(__name__)
         self.app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///apartamentos.sqlite3"
         
-        self.app.add_url_rule('/nuevo',view_func=self.agregar)
+        db.init_app(self.app)
+        
+        self.app.add_url_rule('/',view_func=self.buscarTodos)
+        self.app.add_url_rule('/nuevo',view_func=self.agregar, methods=["GET","POST"])
         
         with self.app.app_context():
             db.create_all()
         
         self.app.run(debug=True)
         
+    def buscarTodos(self):
+        
+        return render_template('mostrarTodos.html', apartamentos=Apartamento.query.all()) 
+        
     def agregar(self):
+        
+        if request.method=="POST":
+            direccion=request.form['direccion']
+            area=request.form['area']
+            edad=request.form['edad']
+            parqueaderos=request.form['parqueaderos']
+            valor=request.form['valor']
+            telefono=request.form['telefono']
+            propietario=request.form['propietario']
+            observaciones=request.form['observaciones']
+            
+            miApartamento=Apartamento(direccion,area,edad,parqueaderos,valor,telefono,propietario,observaciones)
+            
+            db.session.add(miApartamento)
+            db.session.commit()
+            
+            return redirect(url_for('buscarTodos'))
+            
         
         return render_template('nuevoApartamento.html')
     
